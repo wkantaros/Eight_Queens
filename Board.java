b35b4b7aea2f144df3b821e184284ce22e0826a9
Will Kantaros <wkantaros@gmail.com>
2018-07-12 14:53:36 -0400

    Minor improvements to .isCorrect() method

--- Board.java
+++ Board.java
@@ -46,51 +46,40 @@ public class Board
       {
         if (board[q.getRank()][i] == '\u2655' && (i != q.getRank()) && (i != q.getFile()))
           return false;
-      }
-      for(int i = 0; i < 8; i++)
-      {
         if (board[i][q.getFile()] == '\u2655' && (i != q.getRank()) && (i != q.getFile()))
           return false;
       }
       for(int i = -1; i + q.getFile() >= 0 && i + q.getRank() >= 0; i--)
-      {
         if(board[q.getRank() + i][q.getFile() + i] == '\u2655') return false;
-      }
       for(int i = 1; i + q.getFile() <= 7 && i + q.getRank() <= 7; i++)
-      {
         if(board[q.getRank() + i][q.getFile() + i] == '\u2655') return false;
-      }
       for(int i = 1; q.getFile() - i >= 0 && q.getRank() + i <= 7; i++)
-      {
         if(board[q.getRank() + i][q.getFile() - i] == '\u2655') return false;
-      }
       for(int i = 1; q.getFile() + i <= 7 && q.getRank() - i >= 0; i++)
-      {
         if(board[q.getRank() - i][q.getFile() + i] == '\u2655') return false;
-      }
     }
     return true;
   }
 
   public String toString()
   {
-    String theMeat = "";
+    String boardContents = "";
     for(int i = 0; i < 8; i++){
       for(int a = 0; a < 9; a++){
-        theMeat += "  | ";
+        boardContents += "  | ";
         if (a != 8)
-          theMeat += board[i][a];
+          boardContents += board[i][a];
         if (a == 8)
-          theMeat += (i + 1);
+          boardContents += (i + 1);
       }
-      theMeat += "\n";
+      boardContents += "\n";
       if(i != 8)
-        theMeat += "  -----------------------------------------\n";
+        boardContents += "  -----------------------------------------\n";
     }
 
     return  "\n" +
             "     A    B    C    D    E    F    G    H\n" +
             "  -----------------------------------------\n" +
-            theMeat + "\n";
+            boardContents + "\n";
   }
 }
